90c303b5a8e15d9b598703b5aed5a722df822bd6
Eric Ross <eric.ross@hp.com>
2016-10-10 06:48:30 -0700

    Added angular start template

---
+++ angular_template/app.js
@@ -0,0 +1,14 @@
+
+(function () {
+
+var app = angular.module('epr',[]);
+
+app.controller('MainController', function(){
+    this.store = res;
+});
+
+var res = {
+    name:'Eric',
+    grade: 'A'
+};
+})();
