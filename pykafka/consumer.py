9d556f454b194e7a90d2370874c99233b28b60a7
Eric Ross <epr@evross.com>
2017-10-23 13:24:55 -0700

    Updated port

--- pykafka/consumer.py
+++ pykafka/consumer.py
@@ -1,6 +1,5 @@
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer('test',bootstrap_servers='192.168.247.225:32768')
+consumer = KafkaConsumer('test',bootstrap_servers='192.168.247.225:32777')
 for msg in consumer:
-    #print(msg.offset, msg.value)
-    pass
+    print(msg.offset, msg.value)
--- pykafka/producer.py
+++ pykafka/producer.py
@@ -2,10 +2,10 @@ from kafka import KafkaProducer
 from time import sleep
 from time import time
 import timeit
-producer = KafkaProducer(bootstrap_servers='192.168.247.225:32768', acks='all')
+producer = KafkaProducer(bootstrap_servers='192.168.247.225:32776', acks='all')
 
 start = time()
-for x in range(100000):
+for x in range(10):
     msg = b'message: '+str(x)
     producer.send('test', msg)
 print time() - start
